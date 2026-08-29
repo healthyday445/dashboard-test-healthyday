7d5a327394e4461bfa6bd7a93bf366830279a7a7
MishraTanishq619 <mishratanishq619@gmail.com>
2026-08-29 19:39:13 +0530

    enh : added server_timestamp to trackSessionClick

--- netlify/functions/portal-session-clicks.js
+++ netlify/functions/portal-session-clicks.js
@@ -38,6 +38,7 @@ export async function handler(event) {
     await db.collection('portal_session_clicks').add({
       ...body,
       date,
+      server_timestamp: nowIST.toISOString(),
     });
 
     return {
