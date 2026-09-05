26e8505e1c48a28a0594283abb455dbf11726821
MishraTanishq619 <mishratanishq619@gmail.com>
2026-09-05 12:52:01 +0530

    optimisation: font cdns

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,3 @@
-import '@fontsource/outfit/400.css';
-import '@fontsource/outfit/700.css';
-import '@fontsource/playball/400.css';
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import ErrorBoundary from "./components/ErrorBoundary";
